f44d14443020f4e4f4cd0e5b45f4a38fcd05246b
its-my-time <cute.shubham4@gmail.com>
2026-01-19 13:37:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const form = document.getElementById("contactForm");
 const msg = document.getElementById("formMsg");
 
-form.addEventListener("submit", async function(e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
   msg.innerText = "Submitting...";
 
@@ -13,23 +13,27 @@ form.addEventListener("submit", async function(e) {
     message: form.message.value
   };
 
-  try {
-    const response = await fetch(
-      "https://script.google.com/macros/s/AKfycby2HHqO-Ih4rLJe9ZhHK5RCQI-QJiB4wsCYOL_c0yWYWoSnUGkHSnmsJgkyGsYC24RHyQ/exec",
-      { method: "POST", body: JSON.stringify(data) }
-    );
+  fetch("https://script.google.com/macros/s/AKfycby2HHqO-Ih4rLJe9ZhHK5RCQI-QJiB4wsCYOL_c0yWYWoSnUGkHSnmsJgkyGsYC24RHyQ/exec", {
+    method: "POST",
+    redirect: "follow",
+    headers: {
+      "Content-Type": "text/plain;charset=utf-8"
+    },
+    body: JSON.stringify(data)
+  })
+    .then(res => res.json())
+    .then(result => {
+      if (result.status === "success") {
+        msg.innerText = "Thank you for contacting us. We will connect you shortly.";
+        form.reset();
+      } else {
+        msg.innerText = "Something went wrong. Please try again.";
+      }
 
-    const result = await response.json();
-
-    if (result.status === "success") {
-      msg.innerText = "Thank you for contacting us. We will connect you shortly.";
-      form.reset();
-    } else {
-      msg.innerText = "Something went wrong.";
-    }
-  } catch {
-    msg.innerText = "Network error.";
-  }
-
-  setTimeout(() => msg.innerText = "", 5000);
+      setTimeout(() => (msg.innerText = ""), 5000);
+    })
+    .catch(() => {
+      msg.innerText = "Something went wrong. Please try again.";
+      setTimeout(() => (msg.innerText = ""), 5000);
+    });
 });
